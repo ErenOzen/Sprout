c53a2a58473c689d46bfea8646502e7238984455
DilayYigit <dilay.yigit@ug.bilkent.edu.tr>
2021-04-29 03:01:02 +0300

    Parts are edited.

--- app/src/main/java/com/example/sprout/Signup.java
+++ app/src/main/java/com/example/sprout/Signup.java
@@ -3,6 +3,8 @@ package com.example.sprout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.sprout.R;
 
@@ -12,5 +14,13 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        Button signupBtn = (Button) findViewById(R.id.signupBtn);
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
