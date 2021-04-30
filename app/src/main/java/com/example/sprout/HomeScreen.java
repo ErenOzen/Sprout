2a6eb89ae3d8372fb599e1bcc2d731566720d98e
ErenOzen <erenozen@protonmail.com>
2021-04-30 21:25:09 +0300

    Sprout version 0.2 Big update v.2

--- app/src/main/java/com/example/sprout/HomeScreen.java
+++ app/src/main/java/com/example/sprout/HomeScreen.java
@@ -1,8 +1,13 @@
 package com.example.sprout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * This class is the java file for HomeScreen Activity screen / exm file .
@@ -11,9 +16,21 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 public class HomeScreen extends AppCompatActivity {
 
+    private Button logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        logout = (Button) findViewById(R.id.logout_button);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(HomeScreen.this, LoginActivity.class));
+            }
+        });
     }
 }
