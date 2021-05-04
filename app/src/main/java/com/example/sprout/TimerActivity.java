b816380461efd5b606c5824d74e8c9cf3d6bb1fa
ErenOzen <erenozen@protonmail.com>
2021-05-04 10:21:09 +0300

    TimerActivity finished.

--- app/src/main/java/com/example/sprout/TimerActivity.java
+++ app/src/main/java/com/example/sprout/TimerActivity.java
@@ -5,6 +5,7 @@ import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -13,9 +14,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class TimerActivity extends AppCompatActivity {
 
-    RadioGroup radioGroup;
-    RadioButton radioButton;
-    TextView textView;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private TextView textView;
+    private EditText editText;
+    private String activityName;
+    private long activityTime;
+    private int seconds;
+    private String chrobase;
 
     private Chronometer chronometer;
     private long pauseOffset;
@@ -25,15 +31,22 @@ public class TimerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
+        editText = (EditText) findViewById(R.id.timerEditText);
         radioGroup = findViewById(R.id.radioGroup);
         textView = findViewById(R.id.text_view_selected);
+
         Button buttonApply = findViewById(R.id.button_apply);
         buttonApply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                seconds = (int) (chronometer.getBase())/ 1000;
+
+                activityName = editText.getText().toString();
                 int radioId = radioGroup.getCheckedRadioButtonId();
                 radioButton = findViewById(radioId);
-                textView.setText("Your choice: " + radioButton.getText());
+                textView.setText("Category: " + radioButton.getText() + "\nName: " + activityName + "\nTime: " + (activityTime) );
+                //textView.setText(activityName);
             }
         });
 
@@ -54,11 +67,14 @@ public class TimerActivity extends AppCompatActivity {
         if (!running) {
             chronometer.setBase(SystemClock.elapsedRealtime() - pauseOffset);
             chronometer.start();
+
             running = true;
+
         }
     }
     public void pauseChronometer(View v) {
         if (running) {
+            activityTime = ((SystemClock.elapsedRealtime() - chronometer.getBase()) / 1000);
             chronometer.stop();
             pauseOffset = SystemClock.elapsedRealtime() - chronometer.getBase();
             running = false;
@@ -73,6 +89,6 @@ public class TimerActivity extends AppCompatActivity {
         int radioId = radioGroup.getCheckedRadioButtonId();
         radioButton = findViewById(radioId);
         //Toast.makeText(this,"Selected Radio Button: " + radioButton.getText(),Toast.LENGTH_LONG).show();
-
+        //Toast.makeText(TimerActivity.this, activityName, Toast.LENGTH_SHORT).show();
     }
 }
